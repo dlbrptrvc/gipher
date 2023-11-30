1ee1a9a7d3fe6fa7f5c1e9ecd34eb0cf0604b785
dlbrptrvc <37641298+dlbrptrvc@users.noreply.github.com>
2023-11-30 21:45:17 +0100

    Enless scroll added

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-body = document.body;
-html = document.documentElement;
+let counter = 0;
 
-const bottomVisible = () =>
-  html.clientHeight + window.scrollY >=
-  (html.scrollHeight || html.clientHeight);
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      counter++;
+      if (counter >= 2) {
+        observer.unobserve(entry.target);
+        fillPageWithGifs();
+      }
+    }
+  });
+});
 
-console.log("On load:", bottomVisible());
+let offset = 0;
+let limit = 10;
 
 function fillPageWithGifs() {
   fetch(
-    "https://api.giphy.com/v1/gifs/trending?api_key=wLCv5l1DHz3a3580HH8ro4cmSTD816IB&limit=25&offset=0&rating=g&bundle=messaging_non_clips"
+    `https://api.giphy.com/v1/gifs/trending?api_key=wLCv5l1DHz3a3580HH8ro4cmSTD816IB&limit=${limit}&offset=${offset}&rating=g&bundle=messaging_non_clips`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      response.data.forEach((item) => {
-        pasteImage(item.images.original.url);
+      offset += limit;
+      response.data.forEach((item, index) => {
+        let img = pasteImage(item.images.original.url);
+        if (index == limit - 1) {
+          observer.observe(img);
+        }
       });
-      console.log("After gifs:", bottomVisible());
     });
 }
 
-function pasteImage(str) {
+function pasteImage(src) {
   let img = document.createElement("img");
-  img.src = str;
+  img.src = src;
+  img.addEventListener("click", (event) => {
+    navigator.clipboard.writeText(event.target.src);
+    msg.style.display = "inline";
+    msg.style.top = event.clientY + "px";
+    msg.style.left = event.clientX + "px";
+    setTimeout(() => {
+      msg.style.display = "none";
+    }, 1500);
+  });
   images.append(img);
+  return img;
 }
 
 fillPageWithGifs();
